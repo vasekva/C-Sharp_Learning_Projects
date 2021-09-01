7393e752f8d21e42ff151beadc30164277075075
vasekva <vahonin_vasek@mail.ru>
2021-09-02 02:36:19 +0300

    Made a Programm.cs for running the Application and reading data from console

---
+++ Console_Apps/CodeBlogFitness/CodeBlogFitness/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CodeBlogFitness_BL.Controller;
+
+namespace CodeBlogFitness
+{
+    public class Program
+    {
+        public Program()
+        {
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует приложение CodeBlogFitness");
+
+            Console.WriteLine("Введите имя пользователя:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш пол:");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите вашу дату рождения:");
+            var birthDate = DateTime.Parse(Console.ReadLine()); //TODO: переписать на try-parse
+
+            Console.WriteLine("Введите ваш вес:");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите ваш рост:");
+            var height = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate,
+                weight, height);
+            userController.Save();
+        }
+    }
+}
--- Console_Apps/CodeBlogFitness/CodeBlogFitness_BL/Controller/UserController.cs
+++ Console_Apps/CodeBlogFitness/CodeBlogFitness_BL/Controller/UserController.cs
@@ -1,28 +1,62 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using CodeBlogFitness_BL.Model;
 
 namespace CodeBlogFitness_BL.Controller
 {
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
     public class UserController
     {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
         public User User { get; }
 
-        public UserController(User user)
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+        /// <returns>Пользователь приложения. </returns>
+        public UserController()
         {
-            if (user is null)
-            {
-                throw new ArgumentNullException("Пользователь не может быть равен null", nameof(user));
-            }
-        }
-
-        public bool Save()
-        {
-            var formatter = new BinaryFormatter
+            var formatter = new BinaryFormatter();
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
 
+                //TODO: Что делать, если не удалось прочитать пользователя?
+            }
+        }
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController(string userName, string genderName,
+            DateTime birthDate, double weight, double height)
+        {
+            //TODO: Проверка
+
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDate, weight, height);
+        }
+
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
             }
         }
     }
