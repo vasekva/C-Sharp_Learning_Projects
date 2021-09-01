2375d3dd901b85d909f97eee10e0596ba9c1bee3
vasekva <vahonin_vasek@mail.ru>
2021-09-01 06:53:19 +0300

    Created model of 'User'

---
+++ Console_Apps/CodeBlogFitness/CodeBlogFitness_BL/Model/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CodeBlogFitness_BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол
+        /// </summary>
+        /// <param name="name">Название пола</param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Необходимо указать пол!", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
