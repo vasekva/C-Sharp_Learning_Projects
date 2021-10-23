d2371f83493d18d069bca788e01ffb84da659116
vasekva <vahonin_vasek@mail.ru>
2021-10-23 19:55:13 +0300

    Made first laboratory work

---
+++ VeeamAcademy/FirstLabProject/Class_Inheritance/Class_Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class_Inheritance
+{
+    class BaseClass
+    {
+        internal virtual string GetInfo()
+        {
+            return ("This is an info from Base Class");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        internal override string GetInfo()
+        {
+            return ("This is an info from Derived Class");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BaseClass myObject = new DerivedClass();
+
+            Console.WriteLine(myObject.GetInfo());
+        }
+    }
+}
